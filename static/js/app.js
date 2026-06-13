43637fee4c61d686b05324aa3eec8d172b8b8e01
VectorPeak <VectorPeak@users.noreply.github.com>
2026-06-13 13:47:40 +0800

    remove-visible-sidebar-selects

--- static/js/app.js
+++ static/js/app.js
@@ -10,15 +10,15 @@ function bindEvents() {
   els.sidebarNewSessionBtn?.addEventListener('click', createNewSession);
   els.clearHistoryBtn?.addEventListener('click', clearHistory);
   els.sendBtn.addEventListener('click', () => state.inProgress ? cancelStream() : sendMessage());
-  els.scenarioSelect.addEventListener('change', async () => {
+  els.scenarioSelect?.addEventListener('change', async () => {
     if (state.inProgress) cancelStream();
     await applyScenario(els.scenarioSelect.value, true);
   });
   [els.sourceFilter, els.tenantInput, els.datasetInput, els.visibilitySelect, els.roleSelect].forEach(item => {
-    item.addEventListener('change', updateScopeDisplay);
-    item.addEventListener('input', updateScopeDisplay);
+    item?.addEventListener('change', updateScopeDisplay);
+    item?.addEventListener('input', updateScopeDisplay);
   });
-  els.sourceFilter.addEventListener('change', updateCategoryActive);
+  els.sourceFilter?.addEventListener('change', updateCategoryActive);
   els.sessionSearchInput?.addEventListener('input', filterHistory);
   els.chatInput.addEventListener('input', autoResizeInput);
   els.chatInput.addEventListener('keydown', event => {
